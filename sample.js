ecd82cbe6003e622d137a1344183a1814f8e0072
dancxjo <tdreed@gmail.com>
2014-11-18 20:22:23 -0800

    Update sample.js

--- sample.js
+++ sample.js
@@ -1,11 +1,19 @@
-function declare() {}
+function Frame() {
+  this.known = [];
+}
+
+Frame.prototype.declare = function(assertion) {
+  this.known.push(assertion);
+}
+
+var root = new Frame();
+var scope = root;
 
 function le(description) {
   // What does a description entail?
 }
 
 var the = le;
-
 
 function Bridi(selbri, sumtiArray) {
   this.selbri = selbri;
@@ -15,7 +23,7 @@ function Bridi(selbri, sumtiArray) {
 var that = Bridi;
 
 // la djan. cu se risna zo’e
-declare(that(gismu.is-the-heart-of, [undefined, theOneCalled('djan')]));
+scope.declare(that(gismu.is-the-heart-of, [undefined, theOneCalled('djan')]));
 
 // la djan. cu ckaji le ka se risna
-declare(that(gismu.has-the-property, [theOneCalled('djan'), the(property(that(gismu.is-the-heart-of, []))]));
+scope.declare(that(gismu.has-the-property, [theOneCalled('djan'), the(property(that(gismu.is-the-heart-of, []))]));
