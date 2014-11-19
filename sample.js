17bdf8be7c624050b20eb8db0bf2a20fe5c3c727
dancxjo <tdreed@gmail.com>
2014-11-18 20:29:18 -0800

    Update sample.js

--- sample.js
+++ sample.js
@@ -9,11 +9,32 @@ Frame.prototype.declare = function(assertion) {
 var root = new Frame();
 var scope = root;
 
+function Description(article, description) {
+  this.article = article;
+  this.description = description;
+}
+
 function le(description) {
-  // What does a description entail?
+  return new Description('le', description);
 }
 
 var the = le;
+
+function la(description) {
+return new Description('la', description);
+}
+
+
+var theOneCalled = la;
+
+function Gismu(rafsi) {
+  this.rafsi = rafsi;
+}
+
+var gismu = {
+  is_the_heart_of: new Gismu(['risna']),
+  has_the_property: new Gismu(['ckaji'])
+};
 
 function Bridi(selbri, sumtiArray) {
   this.selbri = selbri;
@@ -23,7 +44,7 @@ function Bridi(selbri, sumtiArray) {
 var that = Bridi;
 
 // la djan. cu se risna zo’e
-scope.declare(that(gismu.is-the-heart-of, [undefined, theOneCalled('djan')]));
+scope.declare(that(gismu.is_the_heart_of, [undefined, theOneCalled('djan')]));
 
 // la djan. cu ckaji le ka se risna
-scope.declare(that(gismu.has-the-property, [theOneCalled('djan'), the(property(that(gismu.is-the-heart-of, []))]));
+scope.declare(that(gismu.has_the_property, [theOneCalled('djan'), the(property(that(gismu.is_the_heart_of, []))]));
