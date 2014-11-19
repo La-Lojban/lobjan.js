263ebb56033cc35661b6a6b6065dd7f9a5b34145
dancxjo <tdreed@gmail.com>
2014-11-18 19:59:19 -0800

    Create sample.js

---
+++ sample.js
@@ -0,0 +1,8 @@
+// la djan. cu se risna zo’e
+
+var djan = new NamedThing();
+
+declare(that(gismu_is-the-heart-of, [undefined, theOneCalled(djan)]));
+
+// la djan. cu ckaji le ka se risna
+declare(that(gismu_has-the-property, [djan, the()]));
